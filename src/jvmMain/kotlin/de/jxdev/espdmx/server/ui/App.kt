c1ab0fb9340d39d6ea977fd58ce743dba178c180
Xenox003 <xenox003yt@gmail.com>
2023-06-22 13:51:41 +0200

    Initial Commit

---
+++ src/jvmMain/kotlin/de/jxdev/espdmx/server/ui/App.kt
@@ -0,0 +1,19 @@
+package de.jxdev.espdmx.server.ui
+
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+
+@Composable
+fun App() {
+    var text by remember { mutableStateOf("Hello, World!") }
+
+    MaterialTheme {
+        Button(onClick = {
+            text = "Hello, Desktop!"
+        }) {
+            Text(text)
+        }
+    }
+}
